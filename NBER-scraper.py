29a6be870f5a1e8b94247165ba51d0897855f854
tengj98 <tengj98@gmail.com>
2022-08-02 21:19:45 -0400

    Update NBER-scraper.py

--- NBER-scraper.py
+++ NBER-scraper.py
@@ -4,7 +4,11 @@
 # In[1]:
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
